1606dd039ff400d7e96182ab0ed8bf5e20feefdb
jyh071116 <jyh071116@gmail.com>
2023-09-26 19:49:34 +0900

    feat: isChecked 속성 추가

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -5,21 +5,28 @@ import * as S from "./style";
 const Todo = ({ date }: { date: string }) => {
   const [val, setVal] = useState("");
   const todos = getTodo(date);
+
+  const postTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    window.localStorage.setItem(
+      date,
+      JSON.stringify([
+        { id: todos.length, content: val, isChecked: false },
+        ...todos,
+      ]),
+    );
+    setVal("");
+  };
+
   return (
     <>
       {todos.map((todo) => (
-        <div>{todo.content}</div>
+        <>
+          {todo.isChecked ? <div>이미함</div> : <div>안함</div>}
+          <div>{todo.content}</div>
+        </>
       ))}
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          window.localStorage.setItem(
-            date,
-            JSON.stringify([{ id: todos.length, content: val }, ...todos]),
-          );
-          setVal("");
-        }}
-      >
+      <form onSubmit={(e) => postTodo(e)}>
         <input
           autoFocus
           onChange={(e) => setVal(e.currentTarget.value)}
--- src/interfaces/Todo.interface.ts
+++ src/interfaces/Todo.interface.ts
@@ -1,4 +1,5 @@
 export default interface ITodo {
   id: number;
   content: string;
+  isChecked: boolean;
 }
